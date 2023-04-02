21167f3ec86255f9ebd4259d04786773bc6ce0a8
ranog <jprnogueira@yahoo.com.br>
2023-04-02 19:00:25 -0300

    24. Array

--- Program.cs
+++ Program.cs
@@ -1,20 +1,7 @@
-﻿int temperatura = 29;
+﻿var lista = new int[3];
+lista[0] = 1;
+lista[1] = 2;
+lista[2] = 3;
 
-switch (temperatura)
-{
-    case < 0:
-        Console.WriteLine("Está frio para um caralho!");
-        break;
-    case >= 0 and < 10:
-        Console.WriteLine("Está muito frio!");
-        break;
-    case >= 10 and < 23:
-        Console.WriteLine("Frio!");
-        break;
-    case >= 23 and < 30:
-        Console.WriteLine("Tempo bom!");
-        break;
-    default:
-        Console.WriteLine("Está calor!");
-        break;
-}
+Console.WriteLine(lista[0]);
+Console.WriteLine(lista[2]);
