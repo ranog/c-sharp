e9bfcd01620cdd06c43f36fa9b9b62a27de2a420
ranog <jprnogueira@yahoo.com.br>
2023-04-07 18:06:31 -0300

    35. Exe2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿// Criar um array com 2 posições de inteiros e armazenar em uma variável a soma total da lista.
-// A variável deve ser imprimida no console.
+﻿// Dado uma lista com os itens "2, 8, 3, 10, 5, 4, 7, 9, 1" que vão de 1 a 10, efetuar a soma em duas
+// variáveis, a primeira números de 1 a 5 e a segunda de 6 a 10. Imprimir dos dois resultados.
 
-var array = new int[2];
+var data = new List<int> { 2, 8, 3, 10, 5, 4, 7, 9, 1 };
 
-array[0] = 1;
-array[1] = 2;
+var first_data = data.Where(f => f < 6);
+var second_data = data.Where(f => f > 5);
 
-var array_sum = array.Sum();
+var sum_of_first_data = first_data.Sum();
+var sum_of_second_data = second_data.Sum();
 
-Console.WriteLine($"Resultado é {array_sum}.");
+Console.WriteLine("Resultados das somas:");
+Console.WriteLine($"Soma dos primeiros dados = {sum_of_first_data}");
+Console.WriteLine($"Soma dos segundos dados = {sum_of_second_data}");
