3646b8d890b7ae05dda55a608ac8b814b329b6d7
ranog <jprnogueira@yahoo.com.br>
2023-04-06 22:54:08 -0300

    26. List

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿var lista = new int[4];
+﻿var lista = new List<int>();
 
-for (var i = 0; i < lista.Count(); i++)
-    lista[i] = i;
+for (var i = 0; i < 100; i++)
+    lista.Add(i);
 
 for (var i = 0; i < lista.Count(); i++)
     Console.WriteLine(lista[i]);
