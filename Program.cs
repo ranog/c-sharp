11a055cd23b8946dace09c4e1215e8021d19bba3
ranog <jprnogueira@yahoo.com.br>
2023-04-06 22:58:12 -0300

    27. remove

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿var lista = new List<int>();
 
-for (var i = 0; i < 100; i++)
+for (var i = 10; i > 0; i--)
     lista.Add(i);
+
+lista.RemoveAt(2);
+lista.Remove(5);
 
 for (var i = 0; i < lista.Count(); i++)
     Console.WriteLine(lista[i]);
