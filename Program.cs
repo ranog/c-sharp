d94fa6aea46d081020a30703c93a43590c934979
ranog <jprnogueira@yahoo.com.br>
2023-04-07 17:24:21 -0300

    33. continue and break

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿var i = 1;
-
 while (i <= 10)
 {
     var j = 1;
     while (j <= 10)
     {
+        if (j == 7 || j == 8 || j == 9)
+        {
+            j++;
+            //continue;
+            break;
+        }
         Console.WriteLine($"{i} * {j} = {i * j}");
         j++;
     }
-
     i++;
 }
