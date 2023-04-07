7e13f200770d4c46d1f4dccfa72510d28df23a0e
ranog <jprnogueira@yahoo.com.br>
2023-04-06 23:13:34 -0300

    28. where

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 ﻿var lista = new List<int>();
 
-for (var i = 10; i > 0; i--)
-    lista.Add(i);
+lista.Add(5);
+lista.Add(4);
+lista.Add(1);
+lista.Add(3);
+lista.Add(10);
+lista.Add(2);
+lista.Add(8);
 
-lista.RemoveAt(2);
-lista.Remove(5);
-
+//var sublist = lista.Where(l => l > 5);
+var sublist = new List<int>();
 for (var i = 0; i < lista.Count(); i++)
-    Console.WriteLine(lista[i]);
+    if (lista[i] > 5)
+        sublist.Add(lista[i]);
+
+for (var i = 0; i < sublist.Count(); i++)
+    Console.WriteLine(sublist.ElementAt(i));
+    
+var sublista = lista.Where(l => l > 5 && l < 10);
+for (var i = 0; i < sublista.Count(); i++)
+    Console.WriteLine(sublista.ElementAt(i));
