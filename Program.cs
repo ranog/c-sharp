dd4b73959ce32472d9cf63c43b7b4e52a9fd3f4c
ranog <jprnogueira@yahoo.com.br>
2023-04-10 09:04:47 -0300

    36. Methods

--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
-﻿// Dado uma lista com os itens "2, 8, 3, 10, 5, 4, 7, 9, 1" que vão de 1 a 10, efetuar a soma em duas
-// variáveis, a primeira números de 1 a 5 e a segunda de 6 a 10. Imprimir dos dois resultados.
+﻿ExibeMensagem("Henrique", 500);
+ExibeMensagem("Maria", 1000);
 
-var data = new List<int> { 2, 8, 3, 10, 5, 4, 7, 9, 1 };
-
-var first_data = data.Where(f => f < 6);
-var second_data = data.Where(s => s > 5);
-
-var sum_of_first_data = first_data.Sum();
-var sum_of_second_data = second_data.Sum();
-
-Console.WriteLine("Resultados das somas:");
-Console.WriteLine($"Soma dos primeiros dados = {sum_of_first_data}");
-Console.WriteLine($"Soma dos segundos dados = {sum_of_second_data}");
+void ExibeMensagem(string nome, decimal valor)
+{
+    Console.WriteLine($"O funcionário {nome} recebe {valor}");    
+}
