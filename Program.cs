645d737d19a735baafbd4c33fe4d2256b7dccf60
ranog <jprnogueira@yahoo.com.br>
2023-04-10 11:16:21 -0300

    fix message

--- Program.cs
+++ Program.cs
@@ -3,5 +3,5 @@ ExibeMensagem("Maria", 1000);
 
 void ExibeMensagem(string nome, decimal valor)
 {
-    Console.WriteLine($"O funcionário {nome} recebe {valor}");    
+    Console.WriteLine($"O funcionário {nome} recebe {valor}.");    
 }
