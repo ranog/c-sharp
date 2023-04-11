a0507d83420a925a916e6dfa9fd0bd4cf70b87ea
ranog <jprnogueira@yahoo.com.br>
2023-04-10 22:32:38 -0300

    method return and parameter

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿FormataMensagem("Henrique", 500, out var mensagem1);
-Console.WriteLine(mensagem1);
+﻿var resultado1 = FormataMensagem("Henrique", 499, out var mensagem1);
+if(resultado1)
+    Console.WriteLine(mensagem1);
 
-FormataMensagem("Maria", 1000, out var mensagem2);
-Console.WriteLine(mensagem2);
+var resultado2 = FormataMensagem("Maria", 1000, out var mensagem2);
+if(resultado2)
+    Console.WriteLine(mensagem2);
 
-void FormataMensagem(string nome, decimal valor, out string mensagem)
+bool FormataMensagem(string nome, decimal valor, out string mensagem)
 {
+    mensagem = string.Empty;
+    if (valor < 500)
+        return false;
     mensagem = $"O funcionário {nome} recebe {valor}.";
+    return true;
 }
