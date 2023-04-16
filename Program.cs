64db0dbfd7ecdf702f26b24640461879415fd540
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-04-16 19:35:35 -0300

    44. struct with private methods

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿var endereco = new Endereco(rua: "Rua X", numero: "42", cidade: "Y", estado: "OK");
+﻿var endereco = new Endereco(rua: "Rua X", numero: "42", cidade: "Y", estado: "Ok");
 
 Console.WriteLine(endereco.EnderecoCompleto());
+Console.WriteLine(endereco.Valido);
 
 struct Endereco
 {
@@ -8,6 +9,7 @@ struct Endereco
     public string Numero;
     public string Cidade;
     public string Estado;
+    public bool Valido;
 
     public Endereco(string rua, string numero, string cidade, string estado)
     {
@@ -15,6 +17,19 @@ struct Endereco
         Numero = numero;
         Cidade = cidade;
         Estado = estado;
+        Valido = false;
+        Validar();
+    }
+
+    private void Validar()
+    {
+        if (string.IsNullOrEmpty(Rua) || string.IsNullOrEmpty(Numero) || string.IsNullOrEmpty(Cidade) ||
+            string.IsNullOrEmpty(Estado))
+            Valido = false;
+        else
+        {
+            Valido = true;
+        }
     }
 
     public string EnderecoCompleto()
