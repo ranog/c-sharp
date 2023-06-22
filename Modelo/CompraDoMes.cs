1deba8eef13d430c8fe8069e75783d6223aa8cdf
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-06-22 17:47:12 -0300

    test

--- Modelo/CompraDoMes.cs
+++ Modelo/CompraDoMes.cs
@@ -4,10 +4,7 @@ public class Compras
 {
     public DateTime Data { get; set; }
     public string Mercado { get; set; } = default!;
-    public List<string> Itens
-
-
-    { get; set; }
+    public List<string> Itens{get; set;}
 
     public Compras(DateTime data, string mercado, List<string> itens)
     {
